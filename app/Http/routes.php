3b00cd7d62d1d938a8b4889f3aa32e56d778766d
tgeitz <tgeitz@gmail.com>
2016-01-04 17:12:47 -0600

    added Article model and ArticlesController, and registered articles route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,8 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages/home');
 });
+Route::get('articles', 'ArticlesController@index');
 
 /*
 |--------------------------------------------------------------------------
