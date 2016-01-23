4fa5edc96c2a868e0074022f1cb565257963b5ee
tgeitz <tgeitz@gmail.com>
2016-01-22 19:59:39 -0600

    front page overhaul. changed layout and added divs for my projects

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,18 +48,19 @@
             </div>
 
             <div class="collapse navbar-collapse" id="spark-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/') }}">Home</a></li>
-                </ul>
+                {{--<!-- Left Side Of Navbar -->--}}
+                {{--<ul class="nav navbar-nav">--}}
+                    {{--<li><a href="{{ url('/') }}">Home</a></li>--}}
+                {{--</ul>--}}
 
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
+                    @if (Auth::check())
+                    {{--@if (Auth::guest())--}}
+                        {{--<li><a href="{{ url('/login') }}">Login</a></li>--}}
+                        {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
+                    {{--@else--}}
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                 {{ Auth::user()->name }} <span class="caret"></span>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-{{--<div class="container">--}}
-    {{--<div class="row">--}}
-        {{--<div class="col-md-12">--}}
-            {{--<img src="images/ChimpComputer.jpg" id='main-monkey'>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-{{--</div>--}}
-<div class="col-md-10 col-md-offset-1">
-    <div class="row">
-        <h1>Tom Geitz</h1>
-    </div>
-</div>
-<div class="col-md-10 col-md-offset-1">
-    <div class="row">
-        <p>Web developer based in Chicago.  Writer, multi-instrumentalist, and very likely the best Super Smash Bros player you'll ever meet.</p>
-        <a href="https://twitter.com/tom_geitz" class="btn btn-default">Twitter</a>
-        <a href="https://www.linkedin.com/in/tom-geitz-62b44797" class="btn btn-default">LinkedIn</a>
-        <a href="https://www.github.com/tgeitz" class="btn btn-default">GitHub</a>
-        <a href="/resume" class="btn btn-default">Resume</a>
-    </div>
-</div>
-    <hr/>
-<div class="col-md-10 col-md-offset-1">
-    <div class="row">
-        <h2>Projects</h2>
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-4 col-md-offset-1">
-        <h3><a href="http://www.pmunderground.net/">Project M Underground</a></h3>
-        <h5>A community site for the game <strong>Super Smash Bros.: Project M</strong>.  I help with the back end.</h5>
-    </div>
-    <div class="col-md-4 col-md-offset-1">
-        <h3><a href="https://lit-waters-8964.herokuapp.com/">Place My Order</a></h3>
-        <h5>A restaurant ordering app built with DoneJS.</h5>
-    </div>
-</div>
-<hr/>
-<div class="col-md-10 col-md-offset-1">
-    <div class="row">
-        <h2>Recent Articles</h2>
-        <h3>Coding</h3>
-        <h3>Smash</h3>
-        <h3>Music</h3>
-    </div>
-</div>
 
+    <!-- Header -->
+    <a name="about"></a>
+    <div class="intro-header">
+        <div class="container">
 
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="intro-message">
+                        <h2>PHP Developer from Chicago.  A fresh mind with an appetite.</h2>
+                    </div>
+                    <div class="intro-social">
+                        <ul class="list-inline intro-social-buttons">
+                            <li>
+                                <a href="https://twitter.com/tom_geitz" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                            </li>
+                            <li>
+                                <a href="https://github.com/tgeitz" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                            </li>
+                            <li>
+                                <a href="https://www.linkedin.com/in/tom-geitz-62b44797" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
+                            </li>
+                            <li>
+                                <a href="/resume" class="btn btn-default btn-lg"><i class="fa fa-user fa-fw"></i> <span class="network-name">Resume</span></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- /.container -->
+
+    </div>
+    <!-- /.intro-header -->
+<div class="projects-section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-md-offset-2">
+                <div class="pmu-box">
+                    <img src="../images/projectminverted.jpg" />
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="place-my-order-box">
+                    <img src="../images/knife-fork.png" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
